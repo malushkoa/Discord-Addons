05d08a05c9400b8aac68d4fd90f33dafe557ab04
Artem <137920870+malushkoa@users.noreply.github.com>
2023-07-14 16:51:11 +0300

    Update ChatAliases.plugin.js

--- ChatAliases.plugin.js
+++ ChatAliases.plugin.js
@@ -1,1 +1,4 @@
-
+module.exports = (_ => {
+	const changeLog = {
+		"improved": {
+			"Alias Order": "Automatically sets a higher priority for longer aliases, to avoid short aliases from overwritting longer ones"
