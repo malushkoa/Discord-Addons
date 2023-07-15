9523f5ac437c1ddcbec4ef358f929ef5267446cf
Artem <137920870+malushkoa@users.noreply.github.com>
2023-07-15 14:47:38 +0300

    Update ChatAliases.plugin.js

--- ChatAliases.plugin.js
+++ ChatAliases.plugin.js
@@ -2,3 +2,8 @@ module.exports = (_ => {
 	const changeLog = {
 		"improved": {
 			"Alias Order": "Automatically sets a higher priority for longer aliases, to avoid short aliases from overwritting longer ones"
+}
+	};
+
+	return !window.BDFDB_Global || (!window.BDFDB_Global.loaded && !window.BDFDB_Global.started) ? class {
+		constructor (meta) {for (let key in meta) this[key] = meta[key];}
