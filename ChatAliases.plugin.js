cb9373705d179a08f71b7b610cc575b8cff1e726
Artem <137920870+malushkoa@users.noreply.github.com>
2023-07-16 14:13:56 +0300

    Update ChatAliases.plugin.js

--- ChatAliases.plugin.js
+++ ChatAliases.plugin.js
@@ -7,3 +7,5 @@ module.exports = (_ => {
 
 	return !window.BDFDB_Global || (!window.BDFDB_Global.loaded && !window.BDFDB_Global.started) ? class {
 		constructor (meta) {for (let key in meta) this[key] = meta[key];}
+getName () {return this.name;}
+		getAuthor () {return this.author;}
