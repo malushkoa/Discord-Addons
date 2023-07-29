4f8828e83518e47e6800d77e11c543758c15f3f5
Artem <137920870+malushkoa@users.noreply.github.com>
2023-07-29 19:45:21 +0300

    Update ChatAliases.plugin.js

--- ChatAliases.plugin.js
+++ ChatAliases.plugin.js
@@ -33,3 +33,7 @@ this.downloadLibrary();
 				});
 			}
 			if (!window.BDFDB_Global.pluginQueue.includes(this.name)) window.BDFDB_Global.pluginQueue.push(this.name);
+}
+		start () {this.load();}
+		stop () {}
+		getSettingsPanel () {
