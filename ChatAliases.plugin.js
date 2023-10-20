66ba1c735b292fe7c56e1d9d94d3a77dfd92c509
Artem <137920870+malushkoa@users.noreply.github.com>
2023-10-20 12:32:31 +0300

    Update ChatAliases.plugin.js

--- ChatAliases.plugin.js
+++ ChatAliases.plugin.js
@@ -203,3 +203,5 @@ title: "Config Guide",
 BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.Anchor, {href: "https://regexr.com/", children: BDFDB.LanguageUtils.LanguageStrings.HELP + "?"})
 								]
 							].map(string => BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.FormComponents.FormText, {
+	type: BDFDB.LibraryComponents.FormComponents.FormText.Types.DESCRIPTION,
+								children: string
