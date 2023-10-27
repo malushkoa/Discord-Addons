59aea22fc4676b5ac64444aa417bdab4587ac49a
Artem <137920870+malushkoa@users.noreply.github.com>
2023-10-27 15:04:45 +0300

    Update ChatAliases.plugin.js

--- ChatAliases.plugin.js
+++ ChatAliases.plugin.js
@@ -233,3 +233,4 @@ if (text && this.settings.general.addContextMenu) this.injectItem(e, text);
 			injectItem (e, text) {
 				let [children, index] = BDFDB.ContextMenuUtils.findItem(e.returnvalue, {id: "devmode-copy-id", group: true});
 children.splice(index > -1 ? index : children.length, 0, BDFDB.ContextMenuUtils.createItem(BDFDB.LibraryComponents.MenuItems.MenuGroup, {
+children: BDFDB.ContextMenuUtils.createItem(BDFDB.LibraryComponents.MenuItems.MenuItem, {
